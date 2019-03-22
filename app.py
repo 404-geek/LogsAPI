cb77487bf972107f4645a91900b4afb09055386a
404-geek <pranayd61@gmail.com>
2019-03-22 17:47:22 +0530

    Exception Handling

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask,request,jsonify
-import json,os,io,traceback,sys
-import codecs
+import io,traceback
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -15,13 +14,16 @@ def hello():
             for x in datum:
                 x = x.decode('utf-8')
                 if 'ENTER' in x or 'EXIT' in x:
-                    y = x.split(':')
-                    detail = {}
-                    detail["operation"] = y[0].replace('ENTER', 'ENTRY').split(']')[1]
-                    detail["filename"] = y[1].strip()
-                    detail["line_number"] = y[2].split(' ', 1)[0]
-                    detail["name"] = "anonymous" if y[2].split(' ', 1)[1].strip() == "0" else y[2].split(' ', 1)[1].strip()
-                    lists.append(detail.copy())
+                    try:
+                        y = x.split(':')
+                        detail = {}
+                        detail["operation"] = y[0].replace('ENTER', 'ENTRY').split(']')[1]
+                        detail["filename"] = y[1].strip()
+                        detail["line_number"] = y[2].split(' ', 1)[0]
+                        detail["name"] = "anonymous" if y[2].split(' ', 1)[1].strip() == "0" else y[2].split(' ', 1)[1].strip()
+                        lists.append(detail.copy())
+                    except:
+                        print("Invalid Lines")
 
             if not lists:
                 return not_found("Please Enter Logs File Only")
